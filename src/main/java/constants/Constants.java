ca0619bf0bafe1c8c07c6ba887cfee0ebb59a5e7
KuzminAntton <anttonkuzmin@gmail.com>
2018-11-14 23:25:36 +0300

    fixed xpathes

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -6,9 +6,9 @@ public class Constants {
     public static final String PASS = "anton1488";
     public static final String LOGIN = "life_and_travel_chill";
 
-    public static final String XPATH_FOLLOW_BUTTON = ".//button[text() = 'Follow']";
+    public static final String XPATH_FOLLOW_BUTTON = ".//button[contains(text(), 'Follow')]";
 
-    public static final String XPATH_FOLLOWING_BUTTON = ".//button[text() = 'Following']";
+    public static final String XPATH_FOLLOWING_BUTTON = ".//button[contains(text(), 'Following')]";
 
     public static final String XPATH_LIKE_BUTTON = ".//span[@aria-label = 'Like']";
 
