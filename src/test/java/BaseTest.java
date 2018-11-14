79a5205959abc9427d498b9e064d65ff4f8abf92
KuzminAntton <anttonkuzmin@gmail.com>
2018-11-14 23:51:58 +0300

    added logger

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,7 +1,11 @@
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.Log4jEntityResolver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 
 import java.util.concurrent.TimeUnit;
@@ -11,6 +15,7 @@ public class BaseTest extends Assert {
     private static String driverPath = "driver/";
     public static WebDriver driver;
     protected WebDriverWait wait;
+    final static Logger log = LogManager.getLogger(BaseTest.class);
 
     @BeforeClass
     public void setUp() {
@@ -18,13 +23,18 @@ public class BaseTest extends Assert {
         System.setProperty("webdriver.chrome.driver", driverPath + "chromedriver");
 
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
 //        driver.manage().window().maximize();
     }
 
     public static WebDriver getDriver() {
         return driver;
+    }
+
+    @AfterSuite
+    public void finishMessage() {
+        log.info("Finished tests");
     }
 
 }
